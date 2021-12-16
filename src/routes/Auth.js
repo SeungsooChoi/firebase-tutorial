75ee7349f4101f228f0d2fd81e006c865942e437
SeungsooChoi <tmdtn5389@gmail.com>
2021-12-16 21:27:52 +0900

    feat : login form

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,7 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Auth = () => {
-  return <div>Auth</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onChange = (event) => {
+    const {
+      target: { name, value },
+    } = event;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="Email"
+          required
+          value={email}
+          name="email"
+          onChange={onChange}
+        />
+        <input
+          type="password"
+          placeholder="passowrd"
+          required
+          name="password"
+          value={password}
+          onChange={onChange}
+        />
+        <input type="submit" value="Log In" />
+      </form>
+      <div>
+        <button>Continue with Google</button>
+      </div>
+    </>
+  );
 };
 
 export default Auth;
