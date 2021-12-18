f00532b7aabc91b92e52baf5b502f331cfb331c6
SeungsooChoi <tmdtn5389@gmail.com>
2021-12-18 23:28:56 +0900

    feat : form setting before add firebase db

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [tweet, setTweet] = useState("");
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setTweet(value);
+  };
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <input
+          value={tweet}
+          onChange={onChange}
+          type="text"
+          placeholder="무슨 생각해?"
+          maxLength={100}
+        />
+        <input type="submit" value="future" />
+      </form>
+    </>
+  );
 };
 
 export default Home;
